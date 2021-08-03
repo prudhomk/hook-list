212fd3c831dc9a6fbac548bcdabf849947f5c9cf
Kalan Prudhomme <cleaver9@gmail.com>
2021-08-03 15:11:25 -0700

    created container with hooks

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Village from '../../containers/Village';
+import VillagerDetail from '../../containers/VillagerDetail';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+export default class App extends Component {
+
+  render() {
+    return (
+
+      <Router>
+
+        <Switch>
+          <Route path="/" exact={true}
+            component={Village}
+          />
+
+          <Route path="/:_id" exact={true}
+            component={VillagerDetail}
+          />
+
+        </Switch>
+
+      </Router>
+    );
+  }
 }
--- src/containers/Village.js
+++ src/containers/Village.js
@@ -1,1 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import VillagerList from '../components/VillagerList/VillagerList';
+import { fetchVillager } from '../services/villagerApi';
+
+const Village = () => {
+  const [loading, setLoading] = useState(true);
+  const [villagers, setVillagers] = useState([]);
+
+  useEffect(async () => {
+    const villagers = await fetchVillager();
+    setVillagers(villagers);
+    setLoading(false);
+  }, []);
+
+  if(loading) return <h1>Loading...</h1>;
+  return <VillagerList villagers={villagers} />;
+};
+
+export default Village;
 
